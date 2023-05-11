6ebf4b79f96851c688e191fcbed03cd63cae9f63
jj <joyce.bhps@gmail.com>
2023-05-11 15:00:27 -0700

    my dataset

--- django-mongodb/musiclist/my_spotify_songs/models.py
+++ django-mongodb/musiclist/my_spotify_songs/models.py
@@ -5,6 +5,9 @@ class Songs(models.Model):
     # id field is added automatically.
     # Each field is specified as a class attribute, 
     # and each attribute maps to a database column.
-    title = models.CharField(max_length=70, blank=False, default='')
-    description = models.CharField(max_length=200, blank=False, default='')
-    published = models.BooleanField(default=False)
+    name = models.CharField(max_length=30, blank=False, default='')
+    artist = models.CharField(max_length=20, blank=False, default='')
+    acousticness = models.FloatField(default=0)
+    danceability = models.FloatField(default=0)
+    liveness = models.FloatField(default=0)
+    Favorite = models.BooleanField(default=False)
